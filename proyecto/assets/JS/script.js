21e3e327e6b5ae69441cf258cba652d0b885b860
Nahiara Lucía GOMEZ LOITEY <57732014@ceibal.edu.uy>
2026-06-01 19:13:26 -0300

    Modifique el encabezado

--- proyecto/assets/JS/script.js
+++ proyecto/assets/JS/script.js
@@ -1,5 +1,13 @@
-document.getElementById("btnMenu").addEventListener("click", function () {
+document.getElementById("btnAbrir").addEventListener("click", function () {
 
-    document.querySelector(".menu_principal").style.display = "block";
+    document.querySelector(".menu_principal").style.display = "flex";
+    document.querySelector("btnCerrar").style.display = "flex";
+        document.querySelector("btnAbrir").style.display = "none";
+});
 
+document.getElementById("btnCerrar").addEventListener("click", function () {
+
+        document.querySelector(".menu_principal").style.display = "none";
+    document.getElementById("btnCerrar").style.display = "none";
+    document.getElementById("btnAbrir").style.display = "flex";
 });
