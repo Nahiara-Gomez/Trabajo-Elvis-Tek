3ba55e3dd76a47bc291cf1587e0dcbb14a2b64d3
Nahiara Gomez <ngomezloitey1@gmail.com>
2026-06-01 19:50:47 -0300

    le cambie el nombre al boton de el menu hamburgesa para volver al nombre anterior

--- proyecto/assets/JS/script.js
+++ proyecto/assets/JS/script.js
@@ -1,13 +1,13 @@
-document.getElementById("btnAbrir").addEventListener("click", function () {
+document.getElementById("btnMenu").addEventListener("click", function () {
 
     document.querySelector(".menu_principal").style.display = "flex";
     document.querySelector("btnCerrar").style.display = "flex";
-        document.querySelector("btnAbrir").style.display = "none";
+        document.querySelector("btnMenu").style.display = "none";
 });
 
 document.getElementById("btnCerrar").addEventListener("click", function () {
 
         document.querySelector(".menu_principal").style.display = "none";
     document.getElementById("btnCerrar").style.display = "none";
-    document.getElementById("btnAbrir").style.display = "flex";
+    document.getElementById("btnMenu").style.display = "flex";
 });
