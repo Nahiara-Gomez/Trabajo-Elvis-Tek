d5ed78f728ab27d0fb066ce74c353844c7db464e
nahu-olzr <nahuelolazarri04@gmail.com>
2026-06-07 23:51:28 -0300

    se modifico la tabla de Empleados.html

--- proyecto/assets/JS/tablaEmpleados.js
+++ proyecto/assets/JS/tablaEmpleados.js
@@ -1,5 +1,7 @@
 let filaEditando = null;
 
+const formulario = document.querySelector("form");
+
 document.getElementById("btnAltaUsuario").addEventListener("click", function () {
 
     filaEditando = null;
@@ -19,7 +21,9 @@ document.getElementById("btnCerrarAltaUsuario").addEventListener("click", functi
 
 });
 
-document.getElementById("btnGuardarUsuario").addEventListener("click", function () {
+formulario.addEventListener("submit", function (event) {
+
+    event.preventDefault();
 
     let cedula = document.getElementById("cedula").value;
     let nombre = document.getElementById("nombre").value;
@@ -36,32 +40,25 @@ document.getElementById("btnGuardarUsuario").addEventListener("click", function 
         return;
     }
 
+    const empleado = {
+        cedula: cedula,
+        nombre: nombre,
+        apellido: apellido,
+        cargo: cargo
+    };
+
     if (filaEditando === null) {
 
-        let tabla = document.getElementById("tabla");
-
-        let fila = tabla.insertRow();
-
-        fila.insertCell(0).innerText = cedula;
-        fila.insertCell(1).innerText = nombre;
-        fila.insertCell(2).innerText = apellido;
-        fila.insertCell(3).innerText = cargo;
-
-        let acciones = fila.insertCell(4);
-
-        acciones.innerHTML = `
-            <button onclick="modificar(this)">Modificar</button>
-            <button onclick="borrar(this)">Eliminar</button>
-        `;
+        agregarEmpleado(empleado);
 
     } else {
 
         let celdas = filaEditando.getElementsByTagName("td");
 
-        celdas[0].innerText = cedula;
-        celdas[1].innerText = nombre;
-        celdas[2].innerText = apellido;
-        celdas[3].innerText = cargo;
+        celdas[0].innerText = empleado.cedula;
+        celdas[1].innerText = empleado.nombre;
+        celdas[2].innerText = empleado.apellido;
+        celdas[3].innerText = empleado.cargo;
 
         filaEditando = null;
 
@@ -70,6 +67,51 @@ document.getElementById("btnGuardarUsuario").addEventListener("click", function 
     document.querySelector(".formularioAltaUsuario").style.display = "none";
 
 });
+
+function agregarEmpleado(empleado) {
+
+    let tabla = document.getElementById("tabla");
+
+    let fila = document.createElement("tr");
+
+    let tdCedula = document.createElement("td");
+    tdCedula.innerText = empleado.cedula;
+
+    let tdNombre = document.createElement("td");
+    tdNombre.innerText = empleado.nombre;
+
+    let tdApellido = document.createElement("td");
+    tdApellido.innerText = empleado.apellido;
+
+    let tdCargo = document.createElement("td");
+    tdCargo.innerText = empleado.cargo;
+
+    let tdAcciones = document.createElement("td");
+
+    let btnModificar = document.createElement("button");
+    btnModificar.innerText = "Modificar";
+    btnModificar.onclick = function () {
+        modificar(this);
+    };
+
+    let btnEliminar = document.createElement("button");
+    btnEliminar.innerText = "Eliminar";
+    btnEliminar.onclick = function () {
+        borrar(this);
+    };
+
+    tdAcciones.appendChild(btnModificar);
+    tdAcciones.appendChild(btnEliminar);
+
+    fila.appendChild(tdCedula);
+    fila.appendChild(tdNombre);
+    fila.appendChild(tdApellido);
+    fila.appendChild(tdCargo);
+    fila.appendChild(tdAcciones);
+
+    tabla.appendChild(fila);
+
+}
 
 function borrar(boton) {
 
